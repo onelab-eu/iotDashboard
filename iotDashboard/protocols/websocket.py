3f74d44e16b11f99f536089ad26e156a8c14a15f
Loic Baron <loic.baron@lip6.fr>
2016-01-23 14:50:41 +0100

    Working version of CoAP sending messages to WebSocket from FIT IoT-Lab nodes using IPv6

--- iotDashboard/protocols/websocket.py
+++ iotDashboard/protocols/websocket.py
@@ -1,13 +1,11 @@
 import logging, json
 from tornado import websocket, gen
 
-import txthings.coap as coap
-import txthings.resource as resource
-
 logger = logging.getLogger('iotDashboard.websocket')
 cl = []
 
 class Api(websocket.WebSocketHandler):
+        
     def check_origin(self, origin):
         return True
 
@@ -17,37 +15,18 @@ class Api(websocket.WebSocketHandler):
         logger.info("WebSocket opened (%s)" % self.request.remote_ip)
 
     def on_message(self, message):
-        self.write_message(json.dumps({ "message": "Waiting for changes" }))
-        self.jobs()
+        #self.write_message(json.dumps({ "message": "Waiting for changes" }))
+        logger.info("WebSocket msg received (%s)" % message)
+        self.send_to_clients(message)
 
     def on_close(self):
         if self in cl:
             cl.remove(self)
         logger.info("WebSocket closed (%s)" % self.request.remote_ip)
 
-    def printResponse(self, response):
-        print 'First result: ' + response.payload
-        self.write_message(response.payload)
-
-    def printLaterResponse(self, response):
-        print 'Observe result: ' + response.payload
-        self.write_message(response.payload)
-
-    def noResponse(self, failure):
-        print 'Failed to fetch resource:'
-        print failure
-        self.write_message(failure)
-
     @gen.coroutine
-    def jobs(self):
-        endpoint = resource.Endpoint(None)
-        protocol = coap.Coap(endpoint)
-        request = coap.Message(code=coap.GET)
-        #Send request to "coap://iot.eclipse.org:5683/obs"
-        request.opt.uri_path = ('obs',)
-        request.opt.observe = 0
-        request.remote = ("198.41.30.241", coap.COAP_PORT)
-        d = protocol.request(request, observeCallback=self.printLaterResponse)
-        d.addCallback(self.printResponse)
-        d.addErrback(self.noResponse)
+    def send_to_clients(self, message):
+        for client in cl:
+            if client is not self:
+                client.write_message(json.dumps({ "message": message }))
 
